018b10fd2cc6bf3188dc038d93342fa3ba32296f
Hong Phong <hongphong2000p@gmail.com>
2023-06-23 22:28:07 +0700

    res home

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,8 +1,44 @@
-const Footer = () =>{
-    return (
-        <>
-        footer
-        </>
-    )
-}
-export default Footer
+import { Divider } from "antd";
+import "./footer.scss";
+import { useNavigate } from "react-router-dom";
+const Footer = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="footer-main">
+          <div className="about-us">
+            <span style={{ fontWeight: 600 }}>VỀ CHÚNG TÔI</span>
+            <Divider style={{ borderColor: "grey" }} />
+            <p>Giới Thiệu Về Nhà Sách Windy</p>
+            <p>Hệ Thống Nhà Sách Windy</p>
+            <p>Điều Khoản Sử Dụng</p>
+            <p>Chính Sách Bảo Mật</p>
+            <p>Chính Sách Bán Hàng</p>
+            <p>Phương Thức Vận Chuyển</p>
+          </div>
+          <div className="account">
+            <span style={{ fontWeight: 600 }}>TÀI KHOẢN CỦA TÔI</span>
+            <Divider style={{ borderColor: "grey" }} />
+            <p onClick={() => navigate("/login")}>Đăng nhập</p>
+            <p onClick={() => navigate("/register")}>Tạo tài khoản</p>
+          </div>
+          <div className="support">
+            <span style={{ fontWeight: 600 }}>HỖ TRỢ KHÁCH HÀNG </span>
+            <Divider style={{ borderColor: "grey" }} />
+            <p>Các Câu Hỏi Thường Gặp</p>
+            <p>Chính Sách Đổi/Trả Hàng</p>
+            <p>Quy Định Viết Bình Luận</p>
+          </div>
+          <div className="contact">
+            <span style={{ fontWeight: 600 }}>LIÊN HỆ VỚI CHÚNG TÔI</span>
+            <Divider style={{ borderColor: "grey" }} />
+            <p>Hotline: 12345678</p>
+            <p>Email: nhasachwindy@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Footer;
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,4 +1,3 @@
-import { FilterTwoTone, ReloadOutlined } from "@ant-design/icons";
 import {
   Row,
   Col,
@@ -12,17 +11,82 @@ import {
   Pagination,
   Breadcrumb,
   Carousel,
+  Drawer,
 } from "antd";
 import "./home.scss";
 import { useEffect, useState } from "react";
-import { callGetListBook } from "../../services/api";
+import { callFetchCategory, callGetListBook } from "../../services/api";
+import { AiFillFilter } from "react-icons/ai";
+import { GrPowerReset } from "react-icons/gr";
+import { AiOutlineDown } from "react-icons/ai";
+import { AiOutlineUp } from "react-icons/ai";
 
 const Home = () => {
   const [form] = Form.useForm();
+
   const [total, setTotal] = useState(0);
   const [current, setCurrent] = useState(1);
-  const [pageSize, setPageSize] = useState(10);
+  const [pageSize, setPageSize] = useState(20);
   const [dataBook, setDataBook] = useState("");
+  const [activePrice, setactivePrice] = useState({
+    a: false,
+    b: false,
+    c: false,
+    d: false,
+  });
+  const [listCategory, setlistCategory] = useState([]);
+  const [showMore, setShowMore] = useState(false);
+  const [modalFilter, setModalFilter] = useState(false);
+
+  useEffect(() => {
+    const getListCategory = async () => {
+      let res = await callFetchCategory();
+      if (res && res.data) {
+        setlistCategory(res.data);
+      }
+    };
+    getListCategory();
+  }, []);
+  const numberOfItems = showMore ? listCategory.length : 5;
+
+  const handleSelectPrice = (name) => {
+    if (name === "a") {
+      setactivePrice({
+        a: !activePrice.a,
+        b: false,
+        c: false,
+        d: false,
+      });
+    }
+    if (name === "b") {
+      setactivePrice({
+        a: false,
+        b: !activePrice.b,
+        c: false,
+        d: false,
+      });
+    }
+    if (name === "c") {
+      setactivePrice({
+        a: false,
+        b: false,
+        c: !activePrice.c,
+        d: false,
+      });
+    }
+    if (name === "d") {
+      setactivePrice({
+        a: false,
+        b: false,
+        c: false,
+        d: !activePrice.d,
+      });
+    }
+  };
+
+  const onClose = () => {
+    setModalFilter(false);
+  };
   const getListBook = async () => {
     let res = await callGetListBook(current, pageSize);
     if (res && res.data) {
@@ -30,16 +94,18 @@ const Home = () => {
       setTotal(res.data.meta.total);
     }
   };
-  console.log("current", current);
+
   useEffect(() => {
     getListBook();
-  }, []);
+  }, [current]);
 
   const handleChangeFilter = (changedValues, values) => {
-    console.log(">>> check handleChangeFilter", changedValues, values);
+    //console.log(">>> check handleChangeFilter", changedValues, values);
   };
 
-  const onFinish = (values) => {};
+  const onFinish = (values) => {
+    console.log("check value", values);
+  };
 
   const onChange = (key) => {
     console.log(key);
@@ -87,7 +153,21 @@ const Home = () => {
     textAlign: "center",
     background: "#364d79",
   };
-
+  const handleChangePage = (p, s) => {
+    setCurrent(p);
+  };
+  const handleReset = () => {
+    form.resetFields();
+    setactivePrice({
+      a: false,
+      b: false,
+      c: false,
+      d: false,
+    });
+  };
+  const handleShowMore = () => {
+    setShowMore(!showMore);
+  };
   return (
     <div className="homepage">
       <div className="container">
@@ -97,7 +177,7 @@ const Home = () => {
           items={a}
         />
         <Row style={{ gap: 40 }}>
-          <Col md={4} sm={0} xs={0} className="homepage-left">
+          <Col lg={4} sm={24} md={24} className="homepage-left">
             <Form
               onFinish={onFinish}
               form={form}
@@ -105,75 +185,47 @@ const Home = () => {
                 handleChangeFilter(changedValues, values)
               }
             >
-              <div style={{ fontWeight: 600, fontSize: 16, marginBottom: 20 }}>
+              <div
+                style={{
+                  fontWeight: 600,
+                  fontSize: 16,
+                  marginBottom: 20,
+                }}
+              >
                 Danh mục sản phẩm
               </div>
-              <ul style={{ lineHeight: 2 }}>
-                <li>English Books</li>
-                <li>Sách Tiếng Việt</li>
-              </ul>
-              <Divider />
-              <div
-                className="price"
-                style={{ lineHeight: 3, marginBottom: 20 }}
-              >
-                <span
-                  style={{
-                    fontWeight: 600,
-                    fontSize: 16,
-                    display: "block",
-                  }}
-                >
-                  Giá
-                </span>
-
-                <Button>Dưới 40.000</Button>
-                <Button>40.000 - 120.000</Button>
-                <Button>120.000 - 280.000</Button>
-                <Button>Trên 280.000</Button>
-              </div>
-
-              <Form.Item label="Chọn khoảng giá" labelCol={{ span: 24 }}>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <Form.Item name={["range", "from"]}>
-                    <InputNumber
-                      name="from"
-                      min={0}
-                      placeholder="đ TỪ"
-                      formatter={(value) =>
-                        `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-                      }
-                    />
-                  </Form.Item>
-                  <Form.Item>
-                    <span>-</span>
-                  </Form.Item>
-                  <Form.Item name={["range", "to"]}>
-                    <InputNumber
-                      name="to"
-                      min={0}
-                      placeholder="đ ĐẾN"
-                      formatter={(value) =>
-                        `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-                      }
-                    />
-                  </Form.Item>
-                </div>
-                <div>
-                  <Button
-                    onClick={() => form.submit()}
-                    style={{ width: "100%" }}
-                    type="primary"
-                  >
-                    Áp dụng
-                  </Button>
-                </div>
+              <Form.Item name="category" labelCol={{ span: 24 }}>
+                <Checkbox.Group>
+                  <Row>
+                    {listCategory?.length > 0 &&
+                      listCategory
+                        .slice(0, numberOfItems)
+                        .map((item, index) => {
+                          return (
+                            <Col
+                              span={24}
+                              className="category-group"
+                              key={`item-${index}`}
+                            >
+                              <Checkbox value={item}>{item}</Checkbox>
+                            </Col>
+                          );
+                        })}
+                    {showMore === false ? (
+                      <div onClick={() => handleShowMore()} className="show">
+                        <p>Xem tất cả</p>
+                        <AiOutlineDown style={{ color: "rgb(13, 92, 182)" }} />
+                      </div>
+                    ) : (
+                      <div onClick={() => handleShowMore()} className="show">
+                        <p>Thu gọn</p>
+                        <AiOutlineUp style={{ color: "rgb(13, 92, 182)" }} />
+                      </div>
+                    )}
+                  </Row>
+                </Checkbox.Group>
               </Form.Item>
+
               <Divider />
               <div style={{ fontWeight: 600, fontSize: 16, marginBottom: 20 }}>
                 Đánh giá
@@ -192,10 +244,96 @@ const Home = () => {
                   <span className="ant-rate-text">từ 3 sao</span>
                 </div>
               </Form.Item>
+              <Divider />
+              <Form.Item>
+                <div className="price" style={{ lineHeight: 3 }}>
+                  <span
+                    style={{
+                      fontWeight: 600,
+                      fontSize: 16,
+                      display: "block",
+                    }}
+                  >
+                    Giá
+                  </span>
+
+                  <Button
+                    value={40000}
+                    onClick={() => handleSelectPrice("a")}
+                    className={activePrice.a === true ? "active" : ""}
+                  >
+                    Dưới 40.000
+                  </Button>
+                  <Button
+                    onClick={() => handleSelectPrice("b")}
+                    className={activePrice.b === true ? "active" : ""}
+                  >
+                    {" "}
+                    40.000 - 120.000
+                  </Button>
+                  <Button
+                    onClick={() => handleSelectPrice("c")}
+                    className={activePrice.c === true ? "active" : ""}
+                  >
+                    {" "}
+                    120.000 - 300.000
+                  </Button>
+                  <Button
+                    onClick={() => handleSelectPrice("d")}
+                    className={activePrice.d === true ? "active" : ""}
+                  >
+                    {" "}
+                    Trên 300.000
+                  </Button>
+                </div>
+              </Form.Item>
+
+              <Form.Item label="Chọn khoảng giá từ" labelCol={{ span: 24 }}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Form.Item name="from">
+                    <InputNumber
+                      className="input-number"
+                      min={0}
+                      placeholder="đ"
+                      formatter={(value) =>
+                        `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                      }
+                    />
+                  </Form.Item>
+                </div>
+
+                <div
+                  style={{
+                    gap: 20,
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <Button
+                    onClick={() => form.submit()}
+                    style={{ width: "60%" }}
+                    type="primary"
+                  >
+                    Áp dụng
+                  </Button>
+
+                  <Button
+                    onClick={() => handleReset()}
+                    style={{ width: "30%" }}
+                  >
+                    <GrPowerReset />
+                  </Button>
+                </div>
+              </Form.Item>
             </Form>
           </Col>
 
-          <Col md={19} xs={24} className="homepage-right">
+          <Col lg={19} md={24} className="homepage-right">
             <div className="carousel-homepage">
               <div className="carousel">
                 <Carousel autoplay>
@@ -214,13 +352,11 @@ const Home = () => {
                 </Carousel>
               </div>
               <div className="tabs">
-                <Row>
-                  <Tabs
-                    defaultActiveKey="1"
-                    items={items}
-                    onChange={onChange}
-                  />
-                </Row>
+                <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+                <div className="filter" onClick={() => setModalFilter(true)}>
+                  <span style={{ marginRight: 10 }}>Bộ lọc</span>
+                  <AiFillFilter />
+                </div>
               </div>
             </div>
             <div className="home-list">
@@ -280,10 +416,125 @@ const Home = () => {
                 total={total}
                 pageSize={pageSize}
                 responsive
+                onChange={(p, s) => handleChangePage(p, s)}
               />
             </Row>
           </Col>
         </Row>
+        <Drawer
+          title="Bộ lọc sản phẩm"
+          placement="right"
+          onClose={onClose}
+          open={modalFilter}
+        >
+          <Form
+            onFinish={onFinish}
+            form={form}
+            onValuesChange={(changedValues, values) =>
+              handleChangeFilter(changedValues, values)
+            }
+          >
+            <div
+              style={{
+                fontWeight: 600,
+                fontSize: 16,
+                marginBottom: 20,
+              }}
+            >
+              Danh mục sản phẩm
+            </div>
+            <Form.Item name="category" labelCol={{ span: 24 }}>
+              <Checkbox.Group>
+                <Row>
+                  <Col span={24} className="category-group">
+                    <Checkbox value="A">Truyện cổ tích - Ngụ ngôn</Checkbox>
+                  </Col>
+                  <Col span={24} className="category-group">
+                    <Checkbox value="A">Sách kinh tế</Checkbox>
+                  </Col>
+                  <Col span={24}>
+                    <Checkbox value="A">Sách kinh tế</Checkbox>
+                  </Col>
+                  <Col span={24}>
+                    <Checkbox value="A">Sách kinh tế</Checkbox>
+                  </Col>
+                  <Col span={24}>
+                    <Checkbox value="A">Sách kinh tế</Checkbox>
+                  </Col>
+                  <Col span={24}>
+                    <Checkbox value="A">Sách kinh tế</Checkbox>
+                  </Col>
+                </Row>
+              </Checkbox.Group>
+            </Form.Item>
+
+            <Divider />
+            <div className="price" style={{ lineHeight: 3, marginBottom: 20 }}>
+              <span
+                style={{
+                  fontWeight: 600,
+                  fontSize: 16,
+                  display: "block",
+                }}
+              >
+                Giá
+              </span>
+
+              <Button>Dưới 40.000</Button>
+              <Button>40.000 - 120.000</Button>
+              <Button>120.000 - 280.000</Button>
+              <Button>Trên 280.000</Button>
+            </div>
+
+            <Form.Item label="Chọn khoảng giá từ" labelCol={{ span: 24 }}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Form.Item name={["range", "from"]}>
+                  <InputNumber
+                    className="input-number"
+                    name="from"
+                    min={0}
+                    placeholder="đ"
+                    formatter={(value) =>
+                      `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                    }
+                  />
+                </Form.Item>
+              </div>
+              <div>
+                <Button
+                  onClick={() => form.submit()}
+                  style={{ width: "100%" }}
+                  type="primary"
+                >
+                  Áp dụng
+                </Button>
+              </div>
+            </Form.Item>
+            <Divider />
+            <div style={{ fontWeight: 600, fontSize: 16, marginBottom: 20 }}>
+              Đánh giá
+            </div>
+            <Form.Item labelCol={{ span: 24 }} style={{ cursor: "pointer" }}>
+              <div>
+                <Rate value={5} disabled style={{ fontSize: 12 }} />
+                <span className="ant-rate-text">từ 5 sao</span>
+              </div>
+              <div>
+                <Rate value={4} disabled style={{ fontSize: 12 }} />
+                <span className="ant-rate-text">từ 4 sao</span>
+              </div>
+              <div>
+                <Rate value={3} disabled style={{ fontSize: 12 }} />
+                <span className="ant-rate-text">từ 3 sao</span>
+              </div>
+            </Form.Item>
+          </Form>
+        </Drawer>
       </div>
     </div>
   );
